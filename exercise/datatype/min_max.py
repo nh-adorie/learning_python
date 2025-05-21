fe8425695c5493212aae533d709f9b70e45f4f57
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-05-21 16:36:03 +0700

    Update min_max.py

--- exercise/datatype/min_max.py
+++ exercise/datatype/min_max.py
@@ -5,6 +5,16 @@ def min_max(numbers):
     min = numbers[0]
     max = number[0]
     for number in numbers:
-        if number >= min:
+        if number <= min:
             min = number
+        if number >= max:
+            max = number
+    return min max
+
+numbers = input("Please input your numbers, seperated by comma").split(",")
+output_min, output_max = min_max(numbers)
+print("The biggest number in your list is", output_max, "and the smallest number is ",output_min)
+
+
+        
         

4260a43a20530b32cf986fcb65ea73c48e6a814f
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-05-28 08:14:10 +0000

    blind auction completed

--- project/blind_auction.py
+++ project/blind_auction.py
@@ -10,31 +10,33 @@ while True:
 
     while True:
         try:
-            bid = float(input("How much do you bid? "))
+            bid = float(input("\nHow much do you want to bid? $"))
             break
         except Exception as e:
             print(e)
             print("Please input number only")
 
     dict[name] = bid
-    choice = input("Is there other person who want to bid? (type yes to continue) ").lower()
+    choice = input("\nIs there other person who want to bid? \nType yes to continue \nPress any other key to end and see result!\n").lower()
     if choice != "yes":
         break
     else:
         print("\n"*100)
         
-# look for the biggest bid from the dictionary
+# compare bid from the dictionary
 
-winner = ""
-biggest_bid = 0
-
-for person in dict:
-    if dict[person] > biggest_bid:
-        biggest_bid = dict[person]
-        winner = person
+def find_winner(dict):
+    biggest_bid = 0
+    winner = ""
+    for person in dict:
+        if dict[person] > biggest_bid:
+            biggest_bid = dict[person]
+            winner = person
+    return winner, biggest_bid
 
 print("\n"*100)
-print(f"The winner is {winner} with the bid of {biggest_bid}")
+winner, biggest_bid = find_winner(dict)
+print(f"The winner is {winner} with the bid of {biggest_bid}$")
 
 
 

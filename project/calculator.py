874f70f5c0da5e79149fdc4b4d3d9da21484c083
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-05-29 03:14:49 +0000

    calculator

--- project/calculator.py
+++ project/calculator.py
@@ -19,24 +19,51 @@ operations = {
     "/" : divide,
 }
 
-# Use dictionary to perform the calculation
+# Create function to use dictionary to perform the calculation
 def calculation(num1, num2, opt):
     result = operations[opt](num1,num2)
     return result
 
 # Ask input from user
 while True:
-    num1 = float(input("What is the first number? "))
+    # Ask for num1
+    try:
+        num1 = float(input("What is the first number? "))
+        break
+    except Exception as e:
+        print(e)
+    
+    # Ask for operator
     print("""
     + for plus
     - for substract
     * for multiply
     / for divide      
     """)
-    opt = input("What is the operator? ")
-    num2 = float(input("What is the next number? "))
+    while True:
+        opt = input("What is the operator? ")
+        if opt in ["+","-","*","/"]:
+            break
+        else:
+            print("Please input valid operator ")
 
+# Ask for num2:
+while True:
+    try: 
+        num2 = float(input("What is the next number? "))
+        break
+    except Exception as e:
+        print(e)
+
+
+
+
+
+
+while True:
     is_continue = input(f"""Press 'y' to continue calculate with {result}
     Press 'n' to start a new calculation
     Press 'x' to end the program """)
+    if is_continue == "y":
+
 

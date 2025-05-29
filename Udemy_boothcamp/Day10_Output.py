0333aa381e85997b6a31007ff8e14956ae1a7a2b
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-05-29 02:19:39 +0000

    Day10

--- Udemy_boothcamp/Day10_Output.py
+++ Udemy_boothcamp/Day10_Output.py
@@ -1,6 +1,19 @@
 # format name
-def format_name(first_name,last_name):
-    full_name = first_name.title() + " " + last_name.title()
-    return full_name
-print(format_name("chICken","adOrie"))
+# def format_name(first_name,last_name):
+#     full_name = first_name.title() + " " + last_name.title()
+#     return full_name
+# print(format_name("chICken","adOrie"))
 
+# Leap Year
+
+# def is_leap_year(year):
+#     if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+#         return True
+#     return False
+
+    
+
+# chia hết cho 4 nhưng không chia hết cho 100 hoặc chia hết cho 400 thì là năm nhuận
+# chỉ chia hết cho 100 thì không là năm nhuận
+
+# Docstring

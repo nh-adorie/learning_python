6488a8af294f32d016a58a8d144bdc868aa0473a
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-05-30 06:46:06 +0000

    updated blackjack game

--- project/blackjack_capstone.py
+++ project/blackjack_capstone.py
@@ -47,19 +47,31 @@ def play(who):
     
     return(handed_card, scores)
 
-# Create function to reveal score for user
-def result(user,computer):
-    print(f"Final score! \nComputer: {computer}\nYou: {user}")
-    if computer > 23:
-        print("Computer's cards added up more than 23. You win ☆*: .｡. o(≧▽≦)o .｡.:*☆")
-    elif user > 23:
-        print("Your cards added up more than 23. You lose ψ(｀∇´)ψ")
+# Print line by line across all cards
+def print_cards_inline(card_values):
+    card_lines = [art.poker_card[str(val)].splitlines() for val in card_values]
+    num_lines = len(card_lines[0])    
+    for i in range(num_lines):
+        print("   ".join(card[i] for card in card_lines))
+
+# Create function to reveal result
+def result(user, computer):
+    print(f"\nFinal score!")
+    print(f"Your cards: {user_card} → Total: {user}")
+    print_cards_inline(user_card)
+    print(f"\nComputer's cards: {computer_card} → Total: {computer}")
+    print_cards_inline(computer_card)
+
+    if computer > 21:
+        print("Computer's cards added up more than 21. \nYou win \n☆*: .｡. o(≧▽≦)o .｡.:*☆")
+    elif user > 21:
+        print("Your cards added up more than 21. \nYou lose \nψ(｀∇´)ψ")
     elif computer > user:
         print("You lose ψ(｀∇´)ψ ")
     elif computer < user:
-        print("You win ☆*: .｡. o(≧▽≦)o .｡.:*☆")
+        print("You win \n☆*: .｡. o(≧▽≦)o .｡.:*☆")
     elif computer == user:
-        print("Draw! (❁´◡`❁)")
+        print("Draw! \n(❁´◡`❁)")
         
 # Header
 print(art.blackjack)
@@ -67,33 +79,48 @@ print("Welcome to Blackjack Game! ")
 
 # Play the game!
 
-# Deal cards for user and computer
-user_card, user_scores = play("user")
-computer_card, computer_scores = play("computer")
+is_continue = True
+while is_continue == True: 
 
-# Reveal the card
-print("First card!")
-print(f"You received: {user_card[0]} \n{art.poker_card[str(user_card[0])]}")
-print(f"Computer received: {computer_card[0]} \n{art.poker_card[str(computer_card[0])]}")
+    # Deal cards for user and computer
+    user_card, user_scores = play("user")
+    computer_card, computer_scores = play("computer")
 
-print("Second card!")
-print(f"You received: {user_card[1]} \n{art.poker_card[str(user_card[1])]} \nComputer received: secret\n")
+    # Reveal the card
+    print("First card!")
+    print(f"You received: {user_card[0]} \n{art.poker_card[str(user_card[0])]}")
+    print(f"Computer received: {computer_card[0]} \n{art.poker_card[str(computer_card[0])]}")
+    input("Press Enter to reveal next card...")
 
-if user_scores[1] > 17:
-    while True:
-        pick = input("Do you want to pick up another card, press y to pick, or press n to end \n")
-        if pick not in ["y","n"]:
-            print("Please choose y or n only \n ")
-        if pick == "n":
-            result(user_scores[1],computer_scores[1])
-            break
-        if pick == "y":
-            result(user_scores[2],computer_scores[2])
-            break
-elif user_scores[1] <= 17:
-    print("Final card!")
-    print(f"You received: {user_card[2]} \n{art.poker_card[str(user_card[2])]} \nComputer received: secret\n")
-    result(user_scores[2],computer_scores[2])
+    print("Second card!")
+    print(f"You received: {user_card[1]} \n{art.poker_card[str(user_card[1])]} \nComputer received: secret\n")
+    input("Press Enter to reveal next card...")
+
+    if user_scores[1] > 17:
+        while True:
+            pick = input("Do you want to pick up another card, press y to pick, or press n to end \n").lower()
+            if pick not in ["y","n"]:
+                print("Please choose y or n only \n ")
+            if pick == "n":
+                result(user_scores[1],computer_scores[1])
+                break
+            if pick == "y":
+                result(user_scores[2],computer_scores[2])
+                break
+    elif user_scores[1] <= 17:
+        print("Final card!")
+        print(f"You received: {user_card[2]} \n{art.poker_card[str(user_card[2])]} \nComputer received: secret\n")
+        result(user_scores[2],computer_scores[2])
+    
+    ask = input("\nDo you want to play again? y to play or press any key to end ").lower()
+    if ask != "y":
+        print("Goodbye! ")
+        break
+    else:
+        is_continue = True
+        
+
+
    
     
 

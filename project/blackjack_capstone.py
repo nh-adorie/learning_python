b5242c72bb4649e49ae05770d8c1ba933941a3da
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-05-30 03:28:17 +0000

    Blackjack game

--- project/blackjack_capstone.py
+++ project/blackjack_capstone.py
@@ -1,50 +1,47 @@
-# Blackjack // Play rules
-# If the cards add up > 21 --> Bust - Lose
-# If 2 cards add up < 17 --> Must take the 3rd card
-# J, Q, K = 10
-# A = 1 or 11 (you choose)
-
-
-
-# Chia 1 lá bài, print cả bài của mình và của máy tính
-# Chia 2 lá bài, print của mình --> Nếu là A hỏi xem lấy 1 hay 11
-    # Nếu của mình < 17, chia lá thứ 3, print
-    # Nếu của mình > 17, hỏi có chia nữa không
-        # Nếu chia nữa --> chia lá thứ 3, print --> Nếu là A hỏi xem lấy 1 hay 11
-        # Nếu dừng lại --> dừng
-    # Nếu của máy tính < 17, chia lá thứ 3
-    # Nếu của máy tính > 17 --> random xem có chia tiếp không
-# --> Tính tổng --> Thông báo kết quả 
-    # Nếu có ai > 21 --> người đó thua
-    # Nếu hơn điểm --> thắng
-    # ...
 # import necessary libraries
 import random
 import art
 
-handed_cards = []
 cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
-
-# Create function to deal card
-def deal_card():
+# Create function to deal card for user (including handling A card)
+def user_deal_card():
     card = int(random.choice(cards))
-    handed_cards.append(card)
+    if card == 11:
+        print("\nYou recceive an 'A card'!")    
+        while True:
+            try:
+                choice = int(input("What value do you want your A card to hold, 1 or 11? "))
+                if choice != 1 and choice != 11:
+                    print("Please choose 1 or 11 \n")
+                else:
+                    card = choice
+                    break 
+            except ValueError:
+                print("Please input number 1 or 11 only \n")
+        
     return card
 
-# Create function to sum handed cards
-def sum_cards():
-    for card in handed_cards:
+# Create function to deal card for computer (including random A card choice)
+def computer_deal_card():
+    card = int(random.choice(cards))
+    if card == 11:
+        card = random.choice([1,11])
 
-# Create function to handle A card
-def handle_A_card(card):
-    choice = int(input("What value do you want your A card to hold, 1 or 11? "))
-    while True:
-        if choice == 1 or choice == 11:
-            card == choice
-            break
-        else:
-            print("Please choose 1 or 11 ")
-    return card
+# The program
+print(art.blackjack)
+print("Welcome to Blackjack Game! ")
+
+
+
+# User
+user_sum = 0
+while user_sum < 17:
+    new_card = user_deal_card()
+    user_sum += new_card
+    print(f"You receive a {new_card} card")
+    print(f"Your cards added up to {user_sum} \n")
+
+
 
 
 

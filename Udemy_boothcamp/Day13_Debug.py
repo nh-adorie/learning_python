31a4dcd6a6c2b423adee8622d5d4e20df49dc4ef
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-05-31 03:00:20 +0000

    Day13

--- Udemy_boothcamp/Day13_Debug.py
+++ Udemy_boothcamp/Day13_Debug.py
@@ -14,4 +14,36 @@ try: # Dòng code có thể xảy ra lỗi
 except ValueError: # Nếu xảy ra lỗi này thì làm gì
     print("Please input number ") 
 
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
 
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+year = int(input("year: "))
+print(is_leap(year))
+
+
+# Target is the number up to which we count
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0 or number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)

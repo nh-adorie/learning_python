809e2e7e500cebbf7db93b82192c1d5ffd7456c2
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-05-31 02:28:33 +0000

    Day13

---
+++ Udemy_boothcamp/Day13_Debug.py
@@ -0,0 +1,17 @@
+# Debugging 
+"""
+- Describe the problem
+- Reproduce the bug
+- Play computer
+- Fix the Errors 
+- Print()
+- Using Debugger
+- 
+"""
+
+try: # Dòng code có thể xảy ra lỗi
+    age = int(input("How old are you? "))
+except ValueError: # Nếu xảy ra lỗi này thì làm gì
+    print("Please input number ") 
+
+

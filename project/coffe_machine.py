fcfa36e436b3e6332262f47f4d1a557eec408943
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-06-04 04:39:29 +0000

    im stuck

--- project/coffe_machine.py
+++ project/coffe_machine.py
@@ -4,9 +4,13 @@ import coffee_data
 # coffee_art = coffee_data.menu
 # print(coffee_art)
 
-# List of coffee
+#########
 menu = [coffee for coffee in coffee_data.ingredients]
 stock = coffee_data.stock
+master_recipe = coffee_data.ingredients
+ingredient = [item for item in stock]
+customer_choices = {} 
+revenue = 0
 
 # Create function to check valid number
 def valid_number(prompt):
@@ -21,7 +25,6 @@ def valid_number(prompt):
         except ValueError:
             print("Please input valid number ")
             
-
 # Create function to ask customer their choices
 def ask_for_customer_choice(prompt_type,promt_quantity):
     while True:
@@ -31,58 +34,97 @@ def ask_for_customer_choice(prompt_type,promt_quantity):
             return choice, quantity
         elif choice.lower() == "no":
             return None, None
+        
+        # To restock
+        elif choice.lower() == "restock":
+            restock()
+            return None, None
+        
+        # To print report
+        elif choice.lower() == "report":
+            print(f"""
+                Available ingredient: {stock}
+                Total revenue: {revenue}
+                Items order by customer: {customer_choices}
+                  """)
+
         else:
             print("Sorry we do not serve that yet 😓 Please choose other drinks available in our menu ")
         return ask_for_customer_choice(prompt_type,promt_quantity)
 
-customer_choices = {}
-
-type, quantity = ask_for_customer_choice("What do you want to drink? ", "How many cups? ")
-customer_choices[type] = quantity
-
-while True:
-    more_type, more_quantity = ask_for_customer_choice("\nDo you want anything else? ", "How many cups? ")
-    if more_type is None:
-        break
-    customer_choices[more_type] = more_quantity
-
-print("\nLet me confirm your order: ")
-for type in customer_choices:
-    if customer_choices[type] == 1:
-        print(f"{customer_choices[type]} cup of {type}")
-    else: 
-        print(f"{customer_choices[type]} cups of {type}")
-
-# Check if there sufficiant ingredient for the drinks they choose
-
-needed_water = 0
-needed_milk = 0
-needed_sugar = 0
-needed_coffee_beans = 0
-needed_cream = 0
-
-
-master_recipe = coffee_data.ingredients
-ingredient = [stock for item in stock]
-
-for type in customer_choices:
-    recipe = master_recipe[type]
+# Create program to substract the ingredient used from stock // and restock when enter "restock"
+def stock_management(stock, used):
     for item in ingredient:
-        if item == "water":
-            needed_water += recipe[item] * customer_choices[type]
-        elif item == "sugar":
-            needed_sugar += recipe[item] * customer_choices[type]
-        elif item == "milk":
-            needed_milk += recipe[item] * customer_choices[type]
-        elif item == "coffee_beans":
-            needed_coffee_beans += recipe[item] * customer_choices[type]
-        else:
-            needed_cream += recipe[item]
+        stock[item] -= used[item]
+
+def restock():
+    stock = {
+    "water": 5000,
+    "milk": 5000,
+    "sugar": 500, 
+    "coffee_beans": 500,
+    "cream": 500, 
+    }
+    return stock
+
+# Create function to check if there sufficiant ingredient for the drinks they choose
+
+def check_ingredient(customer_choices):
+    needed_ingredient = {
+    "water": 0,
+    "milk": 0,
+    "sugar": 0,
+    "coffee_beans": 0,
+    "cream": 0,
+}
+
+    for type in customer_choices:
+        recipe = master_recipe[type] 
+        for item in ingredient:
+            needed_ingredient[item] += recipe[item] * customer_choices[type]   
+
+    def is_sufficient(needed):
+        stock_management(stock, needed)
+        return all(stock[item] >= 0 for item in ingredient)
+
+    # If there is enough ingredient --> Confirm and continue
+    # If there is not enough ingredient --> Sorry
+
+    price = coffee_data.coffee_prices
+
+    if is_sufficient(needed_ingredient):
+        print("\nLet me confirm your order: ")
+        
+        for type in customer_choices:
+            revenue += customer_choices[type] * price[type]
+            if customer_choices[type] == 1:
+                print(f"{customer_choices[type]} cup of {type}")
+            else: 
+                print(f"{customer_choices[type]} cups of {type}")
+        print(f"Total: ${revenue}")
+    else:
+        print("Sorry there is not enough ingredient now. Do you want other drink? ")
+
+# The program
+
+
+def main():    
+    type, quantity = ask_for_customer_choice("What do you want to drink? ", "How many cups? ")
+    customer_choices[type] = quantity
+    while True:
+        more_type, more_quantity = ask_for_customer_choice("\nDo you want anything else? ", "How many cups? ")
+        if more_type is None:
+            break
+        customer_choices[more_type] = more_quantity
+
+    check_ingredient(customer_choices)
+
+
+
+# Run the program
+if __name__ == "__main__":
+    main()
     
-print(needed_cream, needed_coffee_beans,needed_milk,needed_sugar,needed_water)
-        
-        
-        
-    
+print(stock)
     
 

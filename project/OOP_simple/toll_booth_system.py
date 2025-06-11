e9539339c96de1f3f7c52bf4440bb5610456e0a6
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-06-11 02:16:00 +0000

    booth

--- project/OOP_simple/toll_booth_system.py
+++ project/OOP_simple/toll_booth_system.py
@@ -55,8 +55,51 @@ class TollBooth:
     def add_vehicle(self,vehicle):
         self.vehicle_list.append(vehicle)
     
+    def show_vehicle_list(self,list):
+        for vehicle in self.vehicle_list:
+            print(f"Type: {type(vehicle).__name__}")
+            vehicle.show_info()
+            print("--")
+      
     def calculate_total_payment(self,vehicle_list):
         for vehicle in vehicle_list:
             self.total_payment += vehicle.pay_fee()
         return self.total_payment
+
+raw_car_list = [
+    {"plate_number": "51A-12345", "seats": 4},
+    {"plate_number": "30A-67890", "seats": 7},
+    {"plate_number": "60B-11223", "seats": 5}
+]
+
+raw_truck_list = [
+    {"plate_number": "61C-99999", "load": 8},
+    {"plate_number": "62C-88888", "load": 12},
+    {"plate_number": "63C-77777", "load": 15}
+]
+
+raw_motor_list = [
+    {"plate_number": "59D-12345", "cc": 125},
+    {"plate_number": "59D-67890", "cc": 150},
+    {"plate_number": "59D-99999", "cc": 100}
+]
+
+booth = TollBooth()
+
+for car_dict in raw_car_list:
+    car_obj = Car(car_dict["plate_number"],car_dict["seats"])
+    booth.add_vehicle(car_obj)
+
+for truck_dict in raw_truck_list:
+    truck_obj = Truck(truck_dict["plate_number"],truck_dict["load"])
+    booth.add_vehicle(car_obj)
+
+for motor_dict in raw_motor_list:
+    motor_obj = Motorbike(motor_dict["plate_number"],motor_dict["cc"])
+    booth.add_vehicle(car_obj)
+
+booth.show_vehicle_list(booth.vehicle_list)
+
+    
+
 

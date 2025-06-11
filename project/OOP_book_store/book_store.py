3b7e6b4a08d744d00710152acba055f2b29c4fa8
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-06-11 08:18:31 +0000

    book store

--- project/OOP_book_store/book_store.py
+++ project/OOP_book_store/book_store.py
@@ -9,15 +9,15 @@ class Book:
     def display_info(self):
         print(f"Title: {self.title}")
         print(f"ID: {self.id}")
-        print(f"Author: {self.auhtor}")
-        print(f"Price: {self.price}")
+        print(f"Author: {self.author}")
+        print(f"Price: ${self.price}")
         print(f"Quantity: {self.quantity}")
     
-    def is_enough_book(self, ordered_qty):
-        if ordered_qty > self.quantity:
+    def is_enough(self, quantity):
+        if quantity > self.quantity:
             return False
         else:
-            self.quantity -= ordered_qty
+            self.quantity -=quantity
             return True
 
 class BookStore:
@@ -29,42 +29,41 @@ class BookStore:
     
     def display_book_list(self):
         for book in self.book_list:
-            book.display_info()
+            print(book.title)
     
-    def display_info_by_name(self, name): # Với name là user input
+    def display_info_by_name(self, name):
+        found = False
         for book in self.book_list:
-            if book.title == name:
+            if book.title.lower() == name:
                 book.display_info()
-            else:
-                print("Book not found ")
-           
-# class Customer:
-#     def __init__(self, id, name, email, orders):
-#         self.id = id #string
-#         self.name = name #string
-#         self.email = email #string
-#         self.orders = orders #obj từ class Orders
+                found = True
+        if not found:
+            print(f"{name} not found")
+            
+class Customer:
+    def __init__(self, name, email):
+        self.name = name #string
+        self.email = email #string
+        self.orders = Orders #obj từ class Orders
     
-# class Orders:
-#     def __init__(self, id, customer):
-#         self.id = id
-#         self.customer = customer # obj từ class Customer
-#         self.ordered_books = [] # list gồm nhiều dict {book: obj Book, quantity: user input int}
-#         self.bill = 0 # tổng giá trị đơn hàng
+class Orders:
+    def __init__(self):
+        self.ordered_books = [] # list gồm nhiều dict {book: obj Book, quantity: user input int}
+        self.bill = 0 # tổng giá trị đơn hàng
     
-#     def add_orders(self, book, quantity):
-#         if book.is_enough(): 
-#             # True: còn đủ sách
-#             self.ordered_books.append({"book": book, "quantity": quantity, "price": book.price})
-#         else:
-#             print("Not enough books ")
+    def add_orders(self, book, quantity): # book phải là obj Book
+        if book.is_enough(): 
+            # True: còn đủ sách
+            self.ordered_books.append({"book": book, "quantity": quantity, "price": book.price})
+        else:
+            print("Not enough books ")
     
-#     def summary_orders(self):
-#         for item in self.ordered_books: # item là 1 dict {book: obj Book, quantity: user input int}
-#             print(f"{item["book"]}: {item["quantity"]}")
-#             self.bill += item["book"].price # item tại ["book"] là obj Book
-#         print(f"Total bill: ${self.bill}")
-#         return self.bill
+    # def summary_orders(self):
+    #     for item in self.ordered_books: # item là 1 dict {book: obj Book, quantity: user input int}
+    #         print(f"{item["book"]}: {item["quantity"]}")
+    #         self.bill += item["book"].price # item tại ["book"] là obj Book
+    #     print(f"Total bill: ${self.bill}")
+    #     return self.bill
 
 
 
--- project/OOP_book_store/main.py
+++ project/OOP_book_store/main.py
@@ -1,11 +1,27 @@
-from book_store import Book, BookStore
+from book_store import Book, BookStore, Orders, Customer
 from raw_data import books_data
+import random
 
-# Lấy dữ liệu từ raw data
+# Lấy dữ liệu sách từ raw data
 store = BookStore()
 for book in books_data:
-    book = Book(books_data["id"],books_data["title"],books_data["author"],books_data["price"],books_data["quantity"])
-    store.append(book)
+    add_book = Book(book["id"],book["title"],book["author"],book["price"],book["quantity"])
+    store.add_book(add_book)
+print("\nBooks available in the store:\n")
+store.display_book_list() 
 
-store.display_book_list()
+
+orders = Orders()
+title = input("\nWhat book are you looking for? ").lower()
+quantity = input(f"\nHow many {title} would you like to buy")
+orders.add_orders(title,quantity)
+
+
+customer_name = input("\nWhat is your name? ")
+customer_email = input("\nWhat is your email address? ")
+customer = Customer(customer_name,customer_email,orders)
+
+
+
+
 

78aadb408d45287fd93bbd75f5a46595ee5e8edc
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-06-18 04:57:45 +0000

    snake

---
+++ project/snake/main.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+from snake import Snake
+import time
+
+# set up screen
+screen = Screen()
+screen.setup(width = 500, height = 500)
+screen.bgcolor("black")
+screen.tracer(0)
+
+# create snake obj
+snake = Snake()
+
+# play 
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+screen.exitonclick()

a83e18c27c75da84d5192e790b18cb6985f55bbd
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-07-01 14:54:29 +0700

    Day24 (#3)
    * til about branch
    
    * The mail merge is amazing
    
    * add more detailed comment

---
+++ project/Mail_Merge/main.py
@@ -0,0 +1,31 @@
+import os
+
+# lấy đường dẫn thư mục đang làm việc (của file main.py này này)
+current_path = os.getcwd()
+print(current_path)
+
+letter_path = "/workspaces/learning_python/project/Mail_Merge/Input/Letters/starting_letter.txt"
+names_path = "/workspaces/learning_python/project/Mail_Merge/Input/Names/invited_names.txt"
+
+# đọc file starting_letter ==> lưu nội dung từ file vào biến "letter_template"
+with open(letter_path) as letter_file:
+    letter_template = letter_file.read()
+
+# đọc file invited_names ==> lưu nội dung từ file vào biến "names_list"
+with open(names_path) as names_file:
+    names_list = names_file.readlines()
+
+# print(type(names_list)) 
+# Khi sử dụng read thì names_list là str // Khi sử dụng readlines thì names_list là list
+
+# thay thế tên trong names_list vào letter_template
+for name in names_list: # vì names_list là list nên có thể lặp như này
+    replace_name = name.strip()
+    personized_letter = letter_template.replace("[name]", replace_name) # thay thế [name] thành tên trong list đó
+
+    # lưu thành file mới
+    output_path = "/workspaces/learning_python/project/Mail_Merge/Output/Ready_to_send"
+    personized_path = os.path.join(output_path,f"letter_for_{replace_name}") 
+    with open(personized_path, "w") as output_letter: 
+        # chỗ này cần để mode w để tạo file mới
+        output_letter.write(personized_letter)

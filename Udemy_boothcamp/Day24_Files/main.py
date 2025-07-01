efbbf8c8630bbc995efe0f18e9f7d1b9015de6a9
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-07-01 07:08:04 +0000

    The mail merge is amazing

--- Udemy_boothcamp/Day24_Files/main.py
+++ Udemy_boothcamp/Day24_Files/main.py
@@ -9,19 +9,38 @@
 # seek(0) sẽ đưa con trỏ chuột về đầu file
 
 
-with open("./my_file.txt", mode = "a+") as file:
-    file.seek(0)
-    content = file.read()
-    file.write("\nNew text.")
-    print(content)
+# with open("./my_file.txt", mode = "a+") as file:
+#     file.seek(0)
+#     content = file.read()
+#     file.write("\nNew text.")
+#     print(content)
 
-with open("./new_file.txt", mode = "w+") as new_file:
-    new_file.write("New file from scratch! ")
-    new_file.seek(0)
-    print(new_file.read())
+# with open("./new_file.txt", mode = "w+") as new_file:
+#     new_file.write("New file from scratch! ")
+#     new_file.seek(0)
+#     print(new_file.read())
 
 # Absolute File Path
 # Relative File Path ./ và ../
 # Working Directory
 
 
+import os
+
+# Lấy đường dẫn thư mục chứa file này (main.py)
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+# Tạo đường dẫn đầy đủ đến my_file.txt và new_file.txt trong cùng thư mục với main.py
+my_file_path = os.path.join(base_path, "my_file.txt")
+new_file_path = os.path.join(base_path, "new_file.txt")
+
+with open(my_file_path, mode="a+") as file:
+    file.seek(0)
+    content = file.read()
+    file.write("\nNew text.")
+    print(content)
+
+with open(new_file_path, mode="w+") as new_file:
+    new_file.write("New file from scratch! ")
+    new_file.seek(0)
+    print(new_file.read())

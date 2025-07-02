8c68977ab426ca4533b41121ebdc8546bb8e6718
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-07-02 04:10:45 +0000

    pandas

--- Udemy_boothcamp/Day25_CSV_Pandas_Libraries/main.py
+++ Udemy_boothcamp/Day25_CSV_Pandas_Libraries/main.py
@@ -3,7 +3,7 @@ import csv
 import pandas
 
 current_path = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(current_path,"weather_data.csv")
+# file_path = os.path.join(current_path,"weather_data.csv")
 
 # with open(file_path, mode = "a") as weather_file:
 #     weather_file.write("\nTuesday,18,Rainy")
@@ -20,3 +20,46 @@ file_path = os.path.join(current_path,"weather_data.csv")
 #             temperatures.append(int(temp))
 
 # print(temperatures)
+
+# data = pandas.read_csv(file_path)
+# print(type(data)) # pandas.core.frame.DataFrame (dataframe ~ table) 
+# print(type(data["temp"])) # pandas.core.series.Series (series ~ list ~ column)
+
+# data_dict = data.to_dict()
+# print(data_dict)
+
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+
+# average_temp = data["temp"].mean()
+# print(average_temp)
+
+# print(f"Maximum temperature: {data['temp'].max()}")
+# print(f"Miniimum temperature: {data['temp'].min()}")
+# print(f"Average temperature: {data['temp'].mean()}")
+
+# Get data in column
+# print(data["condition"])
+# print(data.condition) # the same
+
+# Get data in row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# tuesday = data[data.day == "Tuesday"] # tuesday is a dataframe
+
+# Create new dataframe from scratch
+# data_dict = {
+#     "student" : ["adorie", "foxie", "kitty"],
+#     "score" : ["2", "7", "10"]
+# }
+
+# student_data = pandas.DataFrame(data_dict)
+# print(student_data)
+# student_data_path = os.path.join(current_path,"student_data.csv")
+# student_data.to_csv(student_data_path)
+
+squirrel_path = os.path.join(current_path, "squirrel.csv")
+raw = pandas.read_csv(squirrel_path)
+raw = raw[raw["Primary Fur Color"].dropna()]
+

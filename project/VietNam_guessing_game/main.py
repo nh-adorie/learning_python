d48526a6767a60bfd78f3cebbca123783ed64864
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-07-03 03:14:18 +0000

    new feature: save missed cities as a csv file

--- project/VietNam_guessing_game/main.py
+++ project/VietNam_guessing_game/main.py
@@ -24,16 +24,27 @@ screen.bgpic(image_path)
 cities_path = os.path.join(current_path, "cities.csv")
 data = pandas.read_csv(cities_path)
 
+all_cities = data.city.to_list()
 guessed = []
 
 while len(guessed) < 34:
-    answer = screen.textinput(title = f"{len(guessed)}/34 cities correct", prompt = "What's another city's name? ")
-    if answer is None:
-        break  # nếu nhấn cancel
+    answer = screen.textinput(title = f"{len(guessed)}/34 cities correct", prompt = "What's another city's name? ").title()
+    if answer == "Exit":
+
+        # lưu những thành phố chưa đoán được vào 1 file
+        missing_cities = []
+        for city in all_cities:
+            missing_cities = [city for city in all_cities if city not in guessed]
+            df_missing = pandas.DataFrame(missing_cities, columns=["Missing Cities"])
+            new_path = os.path.join(current_path, "missing_cities.csv")
+            df_missing.to_csv(new_path, index=False)
+        break  
     answer = answer.strip().title()
     
-    for city in data.city:
+    for city in all_cities:
         if city == answer and city not in guessed:
+
+            # hiện tên những thành phố đoán đúng
             guessed.append(city)
             city_data = data[data.city == city]
             x = city_data.x.values[0]
